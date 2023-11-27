a43dbc3562190fe8f2cbfe1fff16fb7972d6b44a
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2023-11-28 00:17:16 +0900

    コントローラ設定

--- Gemfile
+++ Gemfile
@@ -63,3 +63,4 @@ end
 
 gem 'devise'
 gem 'rails-i18n'
+gem 'active_hash'
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,26 @@
 class RecipesController < ApplicationController
   def index
+    @recipe = Recipe.all
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if admin_sigend_in?
+      @recipe.admin_in = current_admin.in
+    end
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end    
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:image, :title, :category_id, :ingredients, :steps)   
+  end  
 end
