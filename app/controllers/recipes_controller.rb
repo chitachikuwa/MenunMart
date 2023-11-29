d4cf560a83ed343e67ff0d52c17f7a4794abfa18
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2023-11-30 00:37:45 +0900

    ルーティング・コントロラー設定

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  before_action :check_admin, only: [:new, :edit, :update]
+  before_action :set_recipe, only: [:show, :edit, :update]
   def index
     @recipes = Recipe.all
   end
@@ -20,11 +22,32 @@ class RecipesController < ApplicationController
   end     
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
+  def edit
+  end  
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path
+    else
+      render :edit
+    end
+  end      
+
   private
+
+  def check_admin
+    unless current_admin == @recipe.admin
+      redirect_to root_path
+    end
+  end  
+
   def recipe_params
     params.require(:recipe).permit(:image, :title, :category_id, :ingredients, :seasonings,:steps)   
+  end  
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end  
 end
