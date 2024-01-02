9af7a36612f18c18ce8e67553d49b2ada21fe938
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2024-01-03 02:04:47 +0900

    検索機能　1/3

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,13 +1,29 @@
 class MenusController < ApplicationController
   def index
-    @menus = Menu.all 
+    @menus = Menu.where
   end
 
   def new
     @selected_date = params[:date].to_date
     @day_of_week = @selected_date.strftime('%A')
+    @menu = Menu.new
+    search_term = params[:recipe_search_term]
+    @recipes = Recipe.where("name LIKE ?", "%#{search_term}%") if search_term.present?
   end
   
   def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to root_path
+    else
+      renndr :new
+    end  
   end  
+
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:date, :name, :description)
+  end
 end
